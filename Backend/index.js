776849ccb5668554ccc51284b9169bd96333bc68
Michael Moutu <moutumichael@gmail.com>
2026-03-31 05:46:46 +1000

    modified env

--- Backend/index.js
+++ Backend/index.js
@@ -42,7 +42,7 @@ app.post("/api/contact", async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
